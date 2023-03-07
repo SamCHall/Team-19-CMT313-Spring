b9da4e139a017b0839b22fb68cfc7ee742c6d63e
Timothy Standen <timothyf1@live.co.uk>
2023-03-07 12:49:38 +0000

    Added model for module table Added table for linking many to many relationship between users and modules.

--- aat/models.py
+++ aat/models.py
@@ -2,6 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 from aat.aat import app, db
 
 
+# Tables for many to many relationships links
+module_user = db.Table(
+    'module_user',
+    db.Column("id", db.Integer, primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
+    db.Column("module_id", db.Integer, db.ForeignKey('module.id'))
+)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
@@ -9,26 +18,48 @@ class User(db.Model):
     email = db.Column(db.String)
     user_type = db.Column(db.String)
 
+    modules = db.relationship(
+        'Module',
+        secondary=module_user,
+        back_populates='user'
+    )
+
     __mapper_args__ = {
         "polymorphic_on": "user_type",
         "polymorphic_identity": "user",
     }
 
+
 class Student(User):
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     course = db.Column(db.String)
     cohort = db.Column(db.Integer)
-    # relationship for modules
+
     # relationship for submissions
 
     __mapper_args__ = {
         "polymorphic_identity": "student",
     }
 
+
 class Staff(User):
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     position = db.Column(db.String)
-    # relationship for modules
+
     __mapper_args__ = {
         "polymorphic_identity": "staff",
     }
+
+
+class Module(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    code = db.Column(db.String)
+
+    user = db.relationship(
+        'User',
+        secondary=module_user,
+        back_populates='modules'
+    )
+
+    # relationship for assignments
