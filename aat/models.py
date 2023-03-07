3fc9a01f3fb7bce733c8df14e72273cfb1526d88
Timothy Standen <timothyf1@live.co.uk>
2023-03-07 12:47:10 +0000

    Added polymorphic to user model

--- aat/models.py
+++ aat/models.py
@@ -7,16 +7,28 @@ class User(db.Model):
     first_name = db.Column(db.String)
     surname = db.Column(db.String)
     email = db.Column(db.String)
+    user_type = db.Column(db.String)
 
+    __mapper_args__ = {
+        "polymorphic_on": "user_type",
+        "polymorphic_identity": "user",
+    }
 
 class Student(User):
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     course = db.Column(db.String)
+    cohort = db.Column(db.Integer)
     # relationship for modules
     # relationship for submissions
 
+    __mapper_args__ = {
+        "polymorphic_identity": "student",
+    }
 
 class Staff(User):
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     position = db.Column(db.String)
     # relationship for modules
+    __mapper_args__ = {
+        "polymorphic_identity": "staff",
+    }
