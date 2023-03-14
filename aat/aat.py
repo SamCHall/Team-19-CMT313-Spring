4fafa449b36eb762d7c8aaa0c138fff9b1b17beb
c1921171 <HallSC3@cardiff.ac.uk>
2023-03-14 14:38:55 +0000

    Removed redundant import

--- aat/aat.py
+++ aat/aat.py
@@ -8,9 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'aa
 
 db = SQLAlchemy(app)
 
-import os
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-
 
 from aat.routes import main
