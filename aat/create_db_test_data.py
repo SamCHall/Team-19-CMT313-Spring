65d0537e7a8107f4ed5d02efa797474a79bfdd59
Timothy Standen <timothyf1@live.co.uk>
2023-03-16 11:25:55 +0000

    Updates to test data as follows:
    - Users name have actual made up names and not what I pressed on the keyborad
    - Test questions for question type 1
    - These questions have been added to an assignment

--- aat/create_db_test_data.py
+++ aat/create_db_test_data.py
@@ -3,95 +3,107 @@ from .models import *
 
 try:
     student1 = Student(
-        username = "rtjst",
+        username = "leia",
         password = "alpha",
         email = "c@example.com",
-        first_name = "Alpha",
-        surname = "1",
+        first_name = "Leia",
+        surname = "Beasley",
         cohort = 2013,
         course = "computing",
     )
     db.session.add(student1)
 
     student2 = Student(
-        username = "ej7u6s",
+        username = "priya",
         password = "alpha",
         email = "d@example.com",
-        first_name = "Beta",
-        surname = "2",
+        first_name = "Priya",
+        surname = "Bishop",
         cohort = 2013,
         course = "computing"
     )
     db.session.add(student2)
 
     student3 = Student(
-        username = "dhtrh",
+        username = "elliot",
         password = "alpha",
         email = "e@example.com",
-        first_name = "Gamma",
-        surname = "3",
+        first_name = "Elliot",
+        surname = "Hull",
         cohort = 2014,
         course = "computing"
     )
     db.session.add(student3)
 
     student4 = Student(
-        username = "dtyhstre",
+        username = "nathaniel",
         password = "alpha",
         email = "f@example.com",
-        first_name = "Delta",
-        surname = "4",
+        first_name = "Nathaniel",
+        surname = "Moore",
         cohort = 2014,
         course = "computing"
     )
     db.session.add(student4)
 
     staff1 = Staff(
-        username = "dttrhyt",
+        username = "elena",
         password = "alpha",
         email = "a@example.com",
-        first_name = "Martin",
-        surname = "Chorley",
+        first_name = "Elena",
+        surname = "Thomas",
         position = "Lect"
     )
     db.session.add(staff1)
 
     staff2 = Staff(
-        username = "ujytyd",
+        username = "oakley",
         password = "alpha",
         email = "b@example.com",
-        first_name = "Matthew",
-        surname = "Morgan",
+        first_name = "Oakley",
+        surname = "Bender",
         position = "Lect"
     )
     db.session.add(staff2)
 
     module1 = Module(
-        name = "Computational Thinking",
-        code = "CMT119",
+        name = "Programming",
+        code = "COMP1-2",
         user = [student1, student2, student3, staff1, staff2]
     )
     db.session.add(module1)
 
     module2 = Module(
-        name = "Fundamentals of Programming",
-        code = "CMT120",
+        name = "Computer systems",
+        code = "COMP2-8",
         user = [student1, student2, student4, staff1]
     )
     db.session.add(module2)
 
     q1 = QuestionType1(
-        title = "hi"
+        title = "Code 1",
+        active = True,
+        question_template = "A {} language program is called {} code.",
+        correct_answers = "['high-level', ' source']",
+        incorrect_answers = "['high', 'low', 'low-level', 'machine', 'object', 'assembly']"
     )
     db.session.add(q1)
 
     q2 = QuestionType1(
-        title = "hello"
+        title = "Code 2",
+        active = True,
+        question_template = "A {} translates source code into {} code.",
+        correct_answers = "['compiler', ' object']",
+        incorrect_answers = "['interpreter', 'assemblers', 'executes', 'machine', 'source']"
     )
     db.session.add(q2)
 
     q3 = QuestionType1(
-        title = "world"
+        title = "Code 3",
+        active = True,
+        question_template = "An {} analyses each {} of the {} code as it {} the statement.",
+        correct_answers = "['interpreter', 'statement', 'source, 'executes']",
+        incorrect_answers = "['compiler', 'assemblers', 'machine', 'object', 'assembly', 'line']"
     )
     db.session.add(q3)
 
@@ -119,21 +131,21 @@ try:
 
     assignment2 = SummativeAssignment(
         title = "Programming Challenges",
-        module = module2,
+        module = module1,
         active = True
     )
     db.session.add(assignment2)
 
     assignment3 = SummativeAssignment(
         title = "Web Application Development",
-        module = module2,
+        module = module1,
         active = True
     )
     db.session.add(assignment3)
 
     assignment4 = FormativeAssignment(
-        title = "Variables - Quiz",
-        module = module1,
+        title = "Classification of software",
+        module = module2,
         active = True
     )
     db.session.add(assignment4)
@@ -154,9 +166,9 @@ try:
 
     db.session.commit()
 
-    assignment1.add_question(q1, 1)
-    assignment1.add_question(q2, 3)
-    assignment1.add_question(q3, 2)
+    assignment4.add_question(q1, 1)
+    assignment4.add_question(q2, 3)
+    assignment4.add_question(q3, 2)
 
     db.session.commit()
 except:
