a157bc26c6511f376244dbfd4b62824715a64426
Timothy Standen <timothyf1@live.co.uk>
2023-03-24 14:30:48 +0000

    Added graph js bundle back in

--- aat/assets.py
+++ aat/assets.py
@@ -5,19 +5,18 @@ bundles = {
         'bootstrap/js/popper.js',
         'bootstrap/js/bootstrap.js',
         'js/script.js',
-        'js/plotly-2.18.2.min.js',
-        'js/display-graph.js',
         # 'js/example.js', <- Copy this format to add a file, making sure to include the comma at the end.
         filters='jsmin',
         output='gen/script.%(version)s.js'
     ),
 
-    # 'graph': Bundle(
-    #     'js/plotly-2.18.2.min.js',
-    #     'js/display-graph.js',
-    #     filters='jsmin',
-    #     output='gen/graph.%(version)s.js'
-    # ),
+    # JS used to graphs, only imported on pages with graphs
+    'graph': Bundle(
+        'js/plotly-2.18.2.min.js',
+        'js/display-graph.js',
+        filters='jsmin',
+        output='gen/graph.%(version)s.js'
+    ),
 
     'css': Bundle(
         'scss/main.scss', # Do not add CSS files here. See instuctions in the SCSS folder.
