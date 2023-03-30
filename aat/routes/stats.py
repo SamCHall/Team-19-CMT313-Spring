ee094a1a00b9a62bbb9173a9f0c05eee5a592a19
Timothy Standen <timothyf1@live.co.uk>
2023-03-30 17:42:50 +0100

    Added section for each question on formative stats

--- aat/routes/stats.py
+++ aat/routes/stats.py
@@ -37,3 +37,22 @@ def assessment_stats(id):
             abort(501)
 
     abort(500)
+
+@app.route('/staff/assessment/<int:id>/stats/<int:question_num>')
+@login_required
+def assessment_question_stats(id, question_num):
+    if current_user.user_type != "staff":
+        abort(403, description="This page can only be accessed by staff.")
+
+    assignment = Assignment.query.filter_by(id=id).first_or_404()
+
+    question = assignment.get_questions()[question_num]
+
+    match question.question_type:
+        case "question_type1":
+            return render_template('stats/assignment_question_type1.html', title=assignment.title, assignment=assignment, question=question, question_number=question_num)
+
+        case "question_type2":
+            return render_template('stats/assignment_question_type2.html', title=assignment.title, assignment=assignment, question=question, question_number=question_num)
+
+    abort(500)
