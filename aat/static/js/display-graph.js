25a0d111678db4f74a86d17a13574eab3b5befdf
Timothy Standen <timothyf1@live.co.uk>
2023-04-06 14:28:44 +0100

    Moving common js code into sepreate function

--- aat/static/js/display-graph.js
+++ aat/static/js/display-graph.js
@@ -1,9 +1,36 @@
 function displayGraph(idName, title, data) {
-    let layout = {title: title}
+    let layout = {title: title};
     let config = {
         displayModeBar: false,
         responsive: true
-    }
+    };
     graph = document.getElementById(idName);
-    Plotly.newPlot(graph, data, layout, config)
+    Plotly.newPlot(graph, data, layout, config);
+}
+
+function barGraphData(data) {
+    return [
+        {
+            x : Object.keys(data),
+            y : Object.values(data),
+            type : 'bar'
+        }
+    ];
+}
+
+function cohortBarGraphData(data) {
+    let displayData = [];
+
+    let i = 0;
+    for ([cohort, mark_dist] of Object.entries(data)) {
+        displayData[i] = {
+            x : Object.keys(mark_dist),
+            y : Object.values(mark_dist),
+            name : cohort,
+            type : 'bar'
+        };
+        i++;
+    }
+
+    return displayData;
 }
